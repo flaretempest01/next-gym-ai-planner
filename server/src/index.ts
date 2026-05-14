56a209e1194627178cc3c19da260933100f186aa
Kaz Sato <kazuyasato01@yahoo.com>
2026-05-15 03:15:19 +0800

    Update index.ts
    add HOST and fix dist file path

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,8 @@ import path from "path";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT: number = parseInt(process.env.PORT || "3001", 10);
+const HOST = "0.0.0.0";
 
 app.use(cors());
 app.use(CookieParser());
@@ -19,14 +20,12 @@ app.use(express.json());
 app.use("/api/profile", profileRouter);
 app.use("/api/plan", planRouter);
 
-// Serve the static files from the Vite build
-app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.static(path.join(__dirname, "../../dist")));
 
-// Handle React routing, return all requests to React app
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../../dist", "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`Server running on port ${PORT}`);
 });
