6844a4cff0ac1b217858bd90ccf5c7105e71c6fb
Kaz Sato <kazuyasato01@yahoo.com>
2026-05-15 03:30:09 +0800

    update DockerFile and index.ts
    modified DockerFile configuration and /server/src/index.ts dist path

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,8 +9,7 @@ import path from "path";
 dotenv.config();
 
 const app = express();
-const PORT: number = parseInt(process.env.PORT || "3001", 10);
-const HOST = "0.0.0.0";
+const distPath = path.join(__dirname, "../../dist");
 
 app.use(cors());
 app.use(CookieParser());
@@ -20,12 +19,13 @@ app.use(express.json());
 app.use("/api/profile", profileRouter);
 app.use("/api/plan", planRouter);
 
-app.use(express.static(path.join(__dirname, "../../dist")));
-
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../dist", "index.html"));
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
+const PORT: number = parseInt(process.env.PORT || "3001", 10);
+const HOST = "0.0.0.0";
+
 app.listen(PORT, HOST, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
