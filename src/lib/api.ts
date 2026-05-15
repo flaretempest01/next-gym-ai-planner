31dd771c40c0559aeb74718c9449276e0162de7d
Kaz Sato <kazuyasato01@yahoo.com>
2026-05-16 01:36:44 +0800

    Update api.ts
    changed local host string value to empty string for mobile web compatibility

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import type { UserProfile } from "../types";
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+const BASE_URL = import.meta.env.VITE_API_URL || "";
 
 async function post(path: string, body: object) {
   const res = await fetch(`${BASE_URL}/api${path}`, {
